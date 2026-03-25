35b05800971df6eb236344be822ff685c3d71da1
Brt <barthunerbeincoding@gmail.com>
2026-03-25 09:21:13 +0100

    refactor: add missing import

--- docenten-api/app/Http/Controllers/Api/CityController.php
+++ docenten-api/app/Http/Controllers/Api/CityController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\City;
 use App\Http\Resources\CityResource;
+use Illuminate\Validation\Rule;
 
 class CityController extends Controller
 {
