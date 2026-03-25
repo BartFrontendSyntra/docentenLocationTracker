d0ccfdedba13df78019e02bdbd64951921ad41a3
Brt <barthunerbeincoding@gmail.com>
2026-03-25 14:06:20 +0100

    Refactor API routes

--- docenten-api/routes/api.php
+++ docenten-api/routes/api.php
@@ -1,28 +1,32 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AddressController;
-use App\Http\Controllers\Api\CertificateController;
-use App\Http\Controllers\Api\CityController;
-use App\Http\Controllers\Api\CourseController;
-use App\Http\Controllers\Api\CourseTypeController;
-use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TeacherController;
+use App\Http\Controllers\Api\CityController;
+use App\Http\Controllers\Api\CourseTypeController;
+use App\Http\Controllers\Api\CertificateController;
+use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\AddressController;
 use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::apiResources([
-    'addresses' => AddressController::class,
-    'certificates' => CertificateController::class,
-    'cities' => CityController::class,
-    'courses' => CourseController::class,
-    'course-types' => CourseTypeController::class,
-    'teachers' => TeacherController::class,
-    'users' => UserController::class,
-]);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/roles', [RoleController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+
+    Route::apiResources([
+        'teachers' => TeacherController::class,
+        'cities' => CityController::class,
+        'course-types' => CourseTypeController::class,
+        'certificates' => CertificateController::class,
+        'roles' => RoleController::class,
+        'courses' => CourseController::class,
+        'addresses' => AddressController::class,
+        'users' => UserController::class,
+    ]);
+});
