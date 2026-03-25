0f3acbc24f1055f56b9beab896f91b5dbd404feb
Brt <barthunerbeincoding@gmail.com>
2026-03-25 14:23:20 +0100

    Add policy classes for Address, Certificate, City, Course, CourseType, Role, Teacher, and User models

---
+++ docenten-api/app/Policies/AddressPolicy.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Address;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class AddressPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return in_array($user->role->name, ['Administrator', 'Viewer']);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Address $address): bool
+    {
+        return in_array($user->role->name, ['Administrator', 'Viewer']);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return in_array($user->role->name, ['Administrator']);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Address $address): bool
+    {
+        return in_array($user->role->name, ['Administrator']);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Address $address): bool
+    {
+        return in_array($user->role->name, ['Administrator']);
+    }
+
+}
