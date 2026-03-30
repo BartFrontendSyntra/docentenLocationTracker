203cb246ee612cfad7ea288c6c9ac7cd563cdcd0
Brt <barthunerbeincoding@gmail.com>
2026-03-30 12:10:34 +0200

    feat: add admin and viewer guard for route protection

---
+++ docenten-frontend/src/app/core/guards/admin-guard.ts
@@ -0,0 +1,27 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '@core/auth/auth-service';
+
+export const adminGuard: CanActivateFn = async (route, state) => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    let user = authService.getCurrentUser();
+
+    if(!user && localStorage.getItem('access_token')) {
+      await authService.fetchProfile();
+      user = authService.getCurrentUser();
+    }
+
+
+    if(!user){
+      return router.parseUrl('/login');
+    }
+
+    if(user()!.role === 'Administrator'){
+      return true;
+    }
+
+    return router.parseUrl('/unauthorized');
+
+};
