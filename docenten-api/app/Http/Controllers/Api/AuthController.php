0b03725738f1aa3deec31e38340c3832a8efa193
Brt <barthunerbeincoding@gmail.com>
2026-03-30 11:55:48 +0200

    fix: update role representation in login response and user resource

--- docenten-api/app/Http/Controllers/Api/AuthController.php
+++ docenten-api/app/Http/Controllers/Api/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
                 'id' => $user->id,
                 'name' => $user->name,
                 'email' => $user->email,
-                'role_id' => $user->role_id,
+                'role' => $user->role->name,
             ]
         ]);
     }
