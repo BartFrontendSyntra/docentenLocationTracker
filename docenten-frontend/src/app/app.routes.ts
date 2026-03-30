bdb14a05742755329b6cef490fe83627cd4f677d
Brt <barthunerbeincoding@gmail.com>
2026-03-30 12:09:27 +0200

    feat: add admin and viewer dashboard routes with route guards

--- docenten-frontend/src/app/app.routes.ts
+++ docenten-frontend/src/app/app.routes.ts
@@ -1,7 +1,22 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
+import { adminGuard } from '@core/guards/admin-guard';
+import { viewerGuard } from '@core/guards/viewer-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
+
+  {
+    path: 'admin-dashboard',
+    // load the admin component
+    canActivate: [adminGuard],
+  },
+  {
+    path: 'viewer-dashboard',
+    // load the viewer component
+     canActivate: [viewerGuard],
+
+  }
+
 ];
