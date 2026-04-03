3e499b483a190cc65bce62408856dc0655bd39c2
Brt <barthunerbeincoding@gmail.com>
2026-04-03 11:35:08 +0200

    bugfix: Point causing utf 8 issue. Point conversion now done by matanyadaev eloquent spatial

--- docenten-api/app/Models/Address.php
+++ docenten-api/app/Models/Address.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use MatanYadaev\EloquentSpatial\Objects\Point;
+use MatanYadaev\EloquentSpatial\Traits\HasSpatial;
 
 class Address extends Model
 {
+    use HasSpatial;
     protected $fillable = ['street', 'house_number', 'city_id', 'location_data'];
+
+    protected $casts = [
+        'location_data' => Point::class,
+    ];
 
     public function city() {
         return $this->belongsTo(City::class);
