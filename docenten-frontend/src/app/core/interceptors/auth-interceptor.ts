7e8b060f05dce0fc9560d41c00454b751c9d78c8
Brt <barthunerbeincoding@gmail.com>
2026-04-03 09:46:07 +0200

    comment for future reference about the use of the interceptor

--- docenten-frontend/src/app/core/interceptors/auth-interceptor.ts
+++ docenten-frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
+// TODO: look at possibility of this being optional if we want a different strategy
+
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const token = localStorage.getItem('access_token');
