f88c82ed3e82ded4727a52cdbf767a2400df1df1
Brt <barthunerbeincoding@gmail.com>
2026-04-16 09:43:43 +0200

    feat: add stat-card and stat-chart components with basic structure

---
+++ docenten-frontend/src/app/shared/components/stat-chart/stat-chart.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stat-chart',
+  imports: [],
+  templateUrl: './stat-chart.html',
+  styleUrl: './stat-chart.css',
+})
+export class StatChart {
+
+}
